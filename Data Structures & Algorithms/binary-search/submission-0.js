44bd2c89a1870d4f0c7fdb9fed1ed53ee80f96f4
Zeeshan Amjad <52594074+zeeshanamjad1080@users.noreply.github.com>
2026-09-23 22:17:09 +0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            let m = Math.floor(l + (r - l) / 2);
+
+            if (nums[m] === target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return -1;
+    }
+}
